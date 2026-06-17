2c4cf043518f220b068990e742f826815bb9d095
DIVYANSHU DUBEY <divyanshudubey2712@gmail.com>
2026-06-17 14:49:12 +0530

    Register rating_review model in admin panel

--- Disease-Prediction-using-Django-and-machine-learning-master/main_app/admin.py
+++ Disease-Prediction-using-Django-and-machine-learning-master/main_app/admin.py
@@ -3,6 +3,7 @@ from .models import patient , doctor , diseaseinfo , consultation,rating_review
 
 # Register your models here.
 
+
 admin.site.register(patient)
 admin.site.register(doctor)
 admin.site.register(diseaseinfo)
